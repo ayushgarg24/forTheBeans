ab109992b93af44cb5eaf5cb4ab0a34bcd0d32d2
mcrchopra <mcrchopra@gmail.com>
2018-02-22 22:55:08 -0800

    new_fft_global_threshold.r
    Tested and Fixed (Still need to fix threshold equation)

--- new_fft_global_threshold.r
+++ new_fft_global_threshold.r
@@ -12,14 +12,15 @@ x <- fft(numeric_data_ts)
 x_abs <- Mod(x)
 x_norm <- x_abs/(length(x_abs)/2)
 
-#Transform FFT to freq by decibels domain (Needs to Be Done)
+#Transform FFT to freq by decibels domain
 x_decNorm <- sapply(x_norm, function(x) 10*log10(x))
 plot(x_decNorm, type = "l")
 
 #Calculate Threshold in Quiet Curve
 get_threshold_in_quiet <- function(f){
-  threshold_equation <- eval((3.64*(f/1000)^(-0.8))-6.5*exp(-0.6((f/1000)-3.3)^2)+((10^-3)*(f/1000)^4))
-  #threshold_equation <- eval((f^2)/10)
+  #threshold_equation <- eval((3.64*(f/1000)^(-0.8))-6.5*exp(-0.6((f/1000)-3.3)^2)+((10^-3)*(f/1000)^4))
+  val <- 4
+  threshold_equation <- eval((f^2))
   #Plot Threshold Equation
   plot(threshold_equation)
   return(threshold_equation)
@@ -28,17 +29,24 @@ get_threshold_in_quiet <- function(f){
 #Remove Rows of Two Dimensional Dataframe Under Curve
 remove_quiet <- function(fft_freq) {
   
+  #Indexer for the Threshold
+  index_list <- 1
+ 
   #Loop through each row of the Transform FFT
   for (i in 1:length(fft_freq)){
     #Call Threshold in Quiet Function
-    if (fft_freq[i] < get_threshold_in_quiet(i)){
-      fft_freq <- fft_freq[-i]
-      i <- i-1
+    if (fft_freq[index_list] < get_threshold_in_quiet(index_list)){
+      print (index_list)
+      fft_freq <- fft_freq[-index_list]
+      #If to convert to 0 instead of deleting
+      #fft_freq[index_list] <- 0
+    }
+    else{
+      index_list <- index_list+1
     }
   }
   return(fft_freq)
 }
-
 
 #Store Final Dataframe with Global Threshold Calculated
 final_dataframe <- remove_quiet(x_decNorm)
