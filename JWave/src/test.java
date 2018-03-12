3ca1aad93d93e42683f9a0588b19896a809f76e0
Alex Radovan <alexradocole@gmail.com>
2018-03-12 00:13:08 -0700

    Testing resampling using built-in methods

--- JWave/src/test.java
+++ JWave/src/test.java
@@ -1,9 +1,14 @@
 import jwave.JWave;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class test {
-    public static void main(String[] args) {
-        JWave a = new JWave();
-        String[] arg = {"Fast", "Wavelet", "Transform", "Daubechies", "2"};
-        a.main(arg);
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
+        AudioInputStream audio = AudioSystem.getAudioInputStream(new File("src/files/inputs/sa.wav"));
+        AudioFormat format = new AudioFormat(44100/10, 16, 2, true, false);
+        AudioInputStream audio2 = AudioSystem.getAudioInputStream(format, audio);
+        AudioSystem.write(audio2, AudioFileFormat.Type.WAVE, new File("src/files/outputs/downsampled.wav"));
     }
 }
